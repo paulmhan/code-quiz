f25fefa8c50b787ae66fa8f35fd6da07267d9cb1
Paul Han <paul.myung.han@gmail.com>
2020-01-29 07:59:58 -0800

    added all quiz questions

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
 const questions = [
     {
-      title: "Which of the following is NOT a javascript data type?",
-      choices: ["Array", "Object", "Table", "String"],
-      answer: "Table"
+        title: "Which of the following is NOT a javascript data type?",
+        choices: ["Array", "Object", "Table", "String"],
+        answer: "Table"
     },
     {
-      title: "Which parenthesis type is used to invoke functions?",
-      choices: ["{}", "[]", "||", "()"],
-      answer: "()"
+        title: "Which parenthesis type is used to invoke functions?",
+        choices: ["{}", "[]", "||", "()"],
+        answer: "()"
     },
     {
-      title: "What will this statement return: console.log('7' == 7)",
-      choices: ["true", "false", "maybe", "depends"],
-      answer: "true"
-    }
-  ];
+        title: "What will this statement return: console.log('7' == 7)",
+        choices: ["true", "false", "maybe", "depends"],
+        answer: "true"
+    },
+    {
+        title: "What will this statement return: console.log('7' === 7)",
+        choices: ["true", "false", "maybe", "depends"],
+        answer: "false"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "Which of these HTML tags do we put our javascript?",
+        choices: ["<js>", "<java>", "<script>", "<javascript>"],
+        answer: "<script>"
+    },
+    {
+        title: "How do you add a comment in Javascript?",
+        choices: ["//", "<!-->", "..", "[]"],
+        answer: "//"
+    },
+    {
+        title: "Which parenthesis type is used to create an array?",
+        choices: ["{}", "[]", "||", "()"],
+        answer: "[]"
+    },
+    {
+        title: "Javascript is the same thing as Java",
+        choices: ["true", "false"],
+        answer: "false"
+    },
+
+
+
+
+
+
+
+
+
+
+
+];
+
+
